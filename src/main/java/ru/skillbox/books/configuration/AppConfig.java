c5843b3c978767f67c2ebf33cb35754e16ee22aa
Marat Murtuzaliev <marat.murtuzaliev@gmail.com>
2024-08-04 13:49:28 +0300

    refactor: remove lettuceConnectionFactory bean

--- src/main/java/ru/skillbox/books/configuration/AppConfig.java
+++ src/main/java/ru/skillbox/books/configuration/AppConfig.java
@@ -3,7 +3,6 @@ package ru.skillbox.books.configuration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -13,14 +12,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class AppConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
-
-    @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(new LettuceConnectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
         return template;
     }
